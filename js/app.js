7d8ed7ceece7c70189518b967e2023abbfa8b917
carlos0027 <carlosdavidyascuaran@gmail.com>
2025-05-05 12:37:56 -0500

    clase 2 java script

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//let numero1= parseInt(prompt("ingrese un numero: "))
+//let numero2= parseInt(prompt("ingrese el numero: "))
+
+let numero1 = 5
+let numero2 = 6
+let suma = numero1+numero2
+let resta = numero1-numero2
+let multiplicacion = numero1*numero2
+let divicion = numero1/numero2
+
+
+//document.writeln("suma: " + (numero1 + numero2 + "<br>"))
+//document.writeln("resta: " + (numero1 - numero2 + "<br>"))
+//document.writeln("multiplicacion: " + (numero1 * numero2 + "<br>"))
+//document.writeln("exponente: " + (numero1 ** numero2 + "<br>"))
+//document.writeln("divicion: " + (numero1 / numero2 + "<br>"))
